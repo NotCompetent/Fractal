412f62405627a7d41b64dfb953aec05a2857b47b
Rameses Santiago <RaSantiago@sfusd.edu>
2020-02-12 08:30:13 -0800

    WOAH STOP

--- Fractal.pde
+++ Fractal.pde
@@ -1,19 +1,31 @@
+int newX = 0;
+int newY = 155;
+int newZ = 255;
+int temp = 0;
 public void setup()   
 {     
-	size(600,600);
+	size(1000,1000);
+	int wow = 1;
+	frameRate(10);
 }   
 
 public void draw()
 {
+	//int wow = wow + 1; 
 	background(0,0,0);
-	fractal(500,500,10,50);
+	fractal(1000,1000,700,0,0,newX,newY,newZ);
+	temp = newX;
+	newX = newY;
+	newY = newZ;
+	newZ = temp;
 }
 
-public void fractal(float x, float y, int big,int border){
-	if(big < 20){
+public void fractal(float x, float y, int big,float border,int blacker,int r, int g, int b){
+	if(big < 10){
+
 		//square
-		fill(255,0,255);
-		stroke(255,0,255);
+		fill(r- blacker,g- blacker,b- blacker);
+		stroke(0- blacker,0- blacker,0- blacker);
 		beginShape();
 		vertex(x+border,y+border);
 		vertex(0+border,y+border);
@@ -25,8 +37,8 @@ public void fractal(float x, float y, int big,int border){
 		fill(0,0,0);
 
 		//inner octagon
-		fill(0,255,255);
-	 	stroke(0,0,255);
+		fill(0- blacker,0- blacker,0- blacker);
+	 	stroke(0- blacker,0- blacker,g- blacker);
 	 	beginShape();
 	 	vertex(border + x*.293,y+border);
 	 	vertex(x + border -x*.293,y+border);
@@ -41,9 +53,39 @@ public void fractal(float x, float y, int big,int border){
 	 	fill(0,0,0);
 
 	 }else{
+
+	 	fill(r- blacker,g- blacker,b- blacker);
+		stroke(0- blacker,0- blacker,0- blacker);
+		beginShape();
+		vertex(x+border,y+border);
+		vertex(0+border,y+border);
+		vertex(0+border,0+border);
+		vertex(x+border,0+border);
+		vertex(x+border,y+border);
+		endShape(CLOSE);
+		stroke(0,0,0);
+		fill(0,0,0);
+
+		//inner octagon
+		fill(0- blacker,0- blacker,0- blacker);
+	 	stroke(0- blacker,0- blacker,g- blacker);
+	 	beginShape();
+	 	vertex(border + x*.293,y+border);
+	 	vertex(x + border -x*.293,y+border);
+	 	vertex(x + border,y+ border -x*.293);
+	 	vertex(x + border,border +x*.293);
+	 	vertex(x + border -x*.293,border);
+	 	vertex(border + x*.293,border);
+	 	vertex(border,border +x*.293);
+	 	vertex(border,y+ border -x*.293);
+	 	endShape(CLOSE);
+	 	stroke(0,0,0);
+	 	fill(0,0,0);
+
+
 	 	//int newX = (int) x*.146;
 	 	//int newY = (int) y*.146;
-	 	fractal(x*.146,y*.146,big-10,border);
+	 	fractal(x-x*.1,y-y*.1,big-10,border +x*.05,blacker + 10,g + 5,b + 5, r + 5);
 	 }
 	
 }
--- build-tmp/source/Fractal.java
+++ build-tmp/source/Fractal.java
@@ -14,22 +14,34 @@ import java.io.IOException;
 
 public class Fractal extends PApplet {
 
+int newX = 0;
+int newY = 155;
+int newZ = 255;
+int temp = 0;
 public void setup()   
 {     
 	
+	int wow = 1;
+	frameRate(10);
 }   
 
 public void draw()
 {
+	//int wow = wow + 1; 
 	background(0,0,0);
-	fractal(500,500,10,50);
+	fractal(1000,1000,700,0,0,newX,newY,newZ);
+	temp = newX;
+	newX = newY;
+	newY = newZ;
+	newZ = temp;
 }
 
-public void fractal(float x, float y, int big,int border){
-	if(big < 20){
+public void fractal(float x, float y, int big,float border,int blacker,int r, int g, int b){
+	if(big < 10){
+
 		//square
-		fill(255,0,255);
-		stroke(255,0,255);
+		fill(r- blacker,g- blacker,b- blacker);
+		stroke(0- blacker,0- blacker,0- blacker);
 		beginShape();
 		vertex(x+border,y+border);
 		vertex(0+border,y+border);
@@ -41,8 +53,8 @@ public void fractal(float x, float y, int big,int border){
 		fill(0,0,0);
 
 		//inner octagon
-		fill(0,255,255);
-	 	stroke(0,0,255);
+		fill(0- blacker,0- blacker,0- blacker);
+	 	stroke(0- blacker,0- blacker,g- blacker);
 	 	beginShape();
 	 	vertex(border + x*.293f,y+border);
 	 	vertex(x + border -x*.293f,y+border);
@@ -57,13 +69,43 @@ public void fractal(float x, float y, int big,int border){
 	 	fill(0,0,0);
 
 	 }else{
+
+	 	fill(r- blacker,g- blacker,b- blacker);
+		stroke(0- blacker,0- blacker,0- blacker);
+		beginShape();
+		vertex(x+border,y+border);
+		vertex(0+border,y+border);
+		vertex(0+border,0+border);
+		vertex(x+border,0+border);
+		vertex(x+border,y+border);
+		endShape(CLOSE);
+		stroke(0,0,0);
+		fill(0,0,0);
+
+		//inner octagon
+		fill(0- blacker,0- blacker,0- blacker);
+	 	stroke(0- blacker,0- blacker,g- blacker);
+	 	beginShape();
+	 	vertex(border + x*.293f,y+border);
+	 	vertex(x + border -x*.293f,y+border);
+	 	vertex(x + border,y+ border -x*.293f);
+	 	vertex(x + border,border +x*.293f);
+	 	vertex(x + border -x*.293f,border);
+	 	vertex(border + x*.293f,border);
+	 	vertex(border,border +x*.293f);
+	 	vertex(border,y+ border -x*.293f);
+	 	endShape(CLOSE);
+	 	stroke(0,0,0);
+	 	fill(0,0,0);
+
+
 	 	//int newX = (int) x*.146;
 	 	//int newY = (int) y*.146;
-	 	fractal(x*.146f,y*.146f,big-10,border);
+	 	fractal(x-x*.1f,y-y*.1f,big-10,border +x*.05f,blacker + 10,g + 5,b + 5, r + 5);
 	 }
 	
 }
-  public void settings() { 	size(600,600); }
+  public void settings() { 	size(1000,1000); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Fractal" };
     if (passedArgs != null) {
